44fb86523e6f6cfb536837664244b66d5d233b59
Jovi <jovimdl@gmail.com>
2019-07-30 17:56:49 -0400

    Mouse and Movement Scripts

---
+++ MouseControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControl : MonoBehaviour
+{
+    public float horizontalSpeed = 60.0f;
+    public float verticalSpeed = 60.0f;
+
+    public float xCorrection = 2.0f;
+    public float yCorrection = 2.0f;
+
+    float mouseX;
+    float mouseY;
+
+    private float xAxisClamp = 0.0f;
+
+    [SerializeField] private Transform playerBody;
+
+    CharacterController characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get the mouse delta, This is not in the range -1 to 1.
+        mouseX = Time.deltaTime * horizontalSpeed * Input.GetAxis("Mouse X");
+        mouseY = Time.deltaTime * verticalSpeed * Input.GetAxis("Mouse Y");
+
+        xAxisClamp += mouseY;
+
+        if(xAxisClamp > 90.0f)
+        {
+            xAxisClamp = 90.0f;
+            mouseY = 0.0f;
+            ClampxAxisRotationToValue(270.0f);
+        }
+        else if(xAxisClamp < -90.0f)
+        {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampxAxisRotationToValue(90.0f);
+        }
+
+
+        transform.Rotate(Vector3.left * mouseY);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+      
+    }
+
+    private void ClampxAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+}
