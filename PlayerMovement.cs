f66d8b308aee29d55ba4ebf24d417846e53eca29
Jovi <jovimdl@gmail.com>
2019-07-31 17:00:31 -0400

    Added Simple Double Jump
    Double Jump lacks redirection of movement while in air. Changed jump function to react to Spacebar pressed down event instead of "is spacebar activated"

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -9,6 +9,9 @@ public class PlayerMovement : MonoBehaviour
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
 
+    private const int maxJump = 3;
+    private int currentJump = 0;
+
     private Vector3 moveDirection = Vector3.zero;
 
     // Start is called before the first frame update
@@ -27,17 +30,24 @@ public class PlayerMovement : MonoBehaviour
             moveDirection = transform.TransformDirection(moveDirection);
             moveDirection *= speed;
 
-            if (Input.GetButton("Jump"))
-            {
-                moveDirection.y = jumpSpeed;
-            }
+            currentJump = 0;
         }
+
+        if (Input.GetKeyDown(KeyCode.Space) && (characterController.isGrounded || maxJump > currentJump))
+        {
+            moveDirection.y = jumpSpeed;
+            currentJump++;
+        }
+
+     
 
         //Apply gravity, which is multiplied by deltaTime twice, once here and again when moveDirection is multiplied
         moveDirection.y -= gravity * Time.deltaTime;
 
         //Move the controller
         characterController.Move(moveDirection * Time.deltaTime);
+
+        Debug.Log("Current Jump value: " + currentJump);
     }
     
 }
